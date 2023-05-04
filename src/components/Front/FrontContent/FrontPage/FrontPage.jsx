ab0238ba5f9fcd9b6d741aa47e0ffb90912e6d93
Dmitry <tahomidev@gmail.com>
2023-05-04 19:49:12 +0300

    Front page refactor

--- src/components/Front/FrontContent/FrontPage/FrontPage.jsx
+++ src/components/Front/FrontContent/FrontPage/FrontPage.jsx
@@ -1,22 +1,111 @@
 import React from "react";
-import s from './FrontPage.module.css';
-import Adwords from "../../Modules/Adwords/Adwrods";
-import Product from "../../FrontContent/FrontPage/Product/Product";
+import s from "./FrontPage.module.css";
+import ProductList from "./Product/ProductList";
+import AdwordSlider from "../../Modules/AdwordSlider/AdwordSlider";
 
-const FrontPage = ()=>{
+const FrontPage = () => {
+    const relatedProductList = [
+        {
+            imgUrl: "",
+            imgAlt: "Мягкая игрушка",
+            title: "Мягкая игрушка мопс-бревно Валера",
+            id: 1,
+            reviews: 17,
+            price: 15,
+            dopPrice: 10,
+        },
+        {
+            imgUrl: "",
+            imgAlt: "Мягкая игрушка",
+            title: "Мягкая игрушка мопс-бревно Валера",
+            id: 2,
+            reviews: 17,
+            price: 15,
+            dopPrice: 10,
+        },
+        {
+            imgUrl: "",
+            imgAlt: "Мягкая игрушка",
+            title: "Мягкая игрушка мопс-бревно Валера",
+            id: 3,
+            reviews: 17,
+            price: 15,
+            dopPrice: 10,
+        },
+        {
+            imgUrl: "",
+            imgAlt: "Мягкая игрушка",
+            title: "Мягкая игрушка мопс-бревно Валера",
+            id: 4,
+            reviews: 17,
+            price: 15,
+            dopPrice: 10,
+        },
+        {
+            imgUrl: "",
+            imgAlt: "Мягкая игрушка",
+            title: "Мягкая игрушка мопс-бревно Валера",
+            id: 5,
+            reviews: 17,
+            price: 15,
+            dopPrice: 10,
+        },
+    ]; // Must be loadRelated()
 
-    return(
+    const firstSliderContent = [
+        {
+            title: "Время осеннего",
+            titleDop: "шопинга!",
+            skidca: "50 %",
+            backUrl: "https://sebweo.com/wp-content/uploads/2019/06/landshaft-bernskikh-alp-v-yasniy-den_thumb.jpg"
+        },
+        {
+            title: "Время осеннего",
+            titleDop: "шопинга!",
+            skidca: "50 %",
+            backUrl: ""
+        },
+        {
+            title: "Время осеннего",
+            titleDop: "шопинга!",
+            skidca: "50 %",
+            backUrl: ""
+        },
+        {
+            title: "Время осеннего",
+            titleDop: "шопинга!",
+            skidca: "50 %",
+            backUrl: ""
+        },
+        {
+            title: "Время осеннего",
+            titleDop: "шопинга!",
+            skidca: "50 %",
+            backUrl: ""
+        },
+        {
+            title: "Время осеннего",
+            titleDop: "шопинга!",
+            skidca: "50 %",
+            backUrl: ""
+        },
+    ];
+
+    return (
         <div className={s.wrap}>
-            <Adwords />
+            <div className={s.slider__wrap}>
+
+                <AdwordSlider data={firstSliderContent} />
+            </div>
             <div className={s.in}>
                 <h3 className={s.title}>Похожие товары</h3>
                 <div className={s.product__in}>
-                    <Product />
+                    <ProductList cards={relatedProductList} />
                 </div>
                 <h3 className={s.title}>Вы смотрели</h3>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default FrontPage
+export default FrontPage;
