3d3e5a89c0e4d8cfd2dd0535ac391d174319c658
Andy <kivenkoandriywork@gmail.com>
2023-05-24 15:11:50 +0300

    Profile Page fix

--- src/components/Front/FrontContent/Pages/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Front/FrontContent/Pages/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from "react";
 import { apiResponse } from "../../../../../functions/api";
 import { NavLink } from "react-router-dom";
 import s from "./ProfileInfo.module.css"
-import pencil from "../../../../../../img/pencilCheck.png"
+import contactsImage from "../../../../../../img/front/contact.png"
+import deliveryImage from "../../../../../../img/front/delivery.png"
 import profileImage from "../../../../../../img/front/profile3.png"
 import arrow from "../../../../../../img/collapse-arrow-50.png"
 import login from "../../../../../../img/front/login.png"
@@ -10,7 +11,10 @@ import login from "../../../../../../img/front/login.png"
 const ProfileInfo = ({item})=>{
     const [showInfo, setShowInfo] = useState({
         firstInfo: true,
-        secondInfo: false
+        secondInfo: false,
+        thirdInfo: false,
+        foursInfo: false,
+        fifthInfo: false
     })
     
     return(
@@ -23,7 +27,6 @@ const ProfileInfo = ({item})=>{
                         <p>Личные данные</p>
                     </div>
                     <div className={s.right}>
-                        <img className={s.pencil} src={pencil} alt="Редактировать" />
                         <img className={`${s.arrow} ${showInfo.firstInfo ? s.arrow__dop : null}`} src={arrow} alt="" onClick={()=>{
                             setShowInfo({...showInfo, firstInfo: !showInfo.firstInfo})
                         }} />
@@ -39,15 +42,11 @@ const ProfileInfo = ({item})=>{
                                 </div>
                                 <div className={s.item}>
                                     <label htmlFor="">Дата рождения</label>
-                                    <input disabled type="text" value={""}/> 
+                                    <input disabled type="text" value={"13.06.1996"}/> 
                                 </div>
                                 <div className={s.item}>
                                     <label htmlFor="">Пол</label>
-                                    <input disabled type="text" value={""}/> 
-                                </div>
-                                <div className={s.item}>
-                                    <label htmlFor="">Язык общения</label>
-                                    <input disabled type="text" value={""}/> 
+                                    <input disabled type="text" value={"Мужской"}/> 
                                 </div>
                             </div>
                         </div>
@@ -63,7 +62,6 @@ const ProfileInfo = ({item})=>{
                         <p>Логин</p>
                     </div>
                     <div className={s.right}>
-                        <img className={s.pencil} src={pencil} alt="Редактировать" />
                         <img className={`${s.arrow} ${showInfo.secondInfo ? s.arrow__dop : null}`} src={arrow} alt="" onClick={()=>{
                             setShowInfo({...showInfo, secondInfo: !showInfo.secondInfo})
                         }} />
@@ -74,12 +72,12 @@ const ProfileInfo = ({item})=>{
                         <div className={s.item__in}>
                             <div className={s.item__info}>
                                 <div className={s.item}>
-                                    <label htmlFor="">Телефон пользователя</label>
-                                    <input disabled type="text" value={item.phone}/> 
+                                    <label htmlFor="">Логин(почта)</label>
+                                    <input disabled type="text" value={item.email}/> 
                                 </div>
                                 <div className={s.item}>
-                                    <label htmlFor="">Email пользователя</label>
-                                    <input disabled type="text" value={item.email}/> 
+                                    <label htmlFor="">Логие(телефон)</label>
+                                    <input disabled type="text" value={item.phone}/> 
                                 </div>
                                 <div className={s.item}>
                                     <label htmlFor="">Тип пользователя</label>
@@ -90,7 +88,81 @@ const ProfileInfo = ({item})=>{
                     : null
                 }
             </div>
+
+            <div className={s.item__wrap}>
+                <div className={s.item__title}>
+                    <div className={s.left}>
+                        <img className={s.profile} src={contactsImage} alt="Профиль" />
+                        <p>Контакты</p>
+                    </div>
+                    <div className={s.right}>
+                        <img className={`${s.arrow} ${showInfo.thirdInfo ? s.arrow__dop : null}`} src={arrow} alt="" onClick={()=>{
+                            setShowInfo({...showInfo, thirdInfo: !showInfo.thirdInfo})
+                        }} />
+                    </div>
+                </div>
+                {
+                    showInfo.thirdInfo ? 
+                        <div className={s.item__in}>
+                            <div className={s.item__info}>
+                                <div className={s.item}>
+                                    <label htmlFor="">Почта</label>
+                                    <input disabled type="text" value={item.email}/> 
+                                </div>
+                                <div className={s.item}>
+                                    <label htmlFor="">Телефон</label>
+                                    <input disabled type="text" value={item.phone}/> 
+                                </div>
+                                <div className={s.item}>
+                                    <label htmlFor="">Дополнительный телефон</label>
+                                    <input disabled type="text" value={"0688562345"}/> 
+                                </div>
+                            </div>
+                        </div>
+                    : null
+                }
+            </div>
+
+            <div className={s.item__wrap}>
+                <div className={s.item__title}>
+                    <div className={s.left}>
+                        <img className={s.profile} src={deliveryImage} alt="Профиль" />
+                        <p>Адрес доставки</p>
+                    </div>
+                    <div className={s.right}>
+                        <img className={`${s.arrow} ${showInfo.foursInfo ? s.arrow__dop : null}`} src={arrow} alt="" onClick={()=>{
+                            setShowInfo({...showInfo, foursInfo: !showInfo.foursInfo})
+                        }} />
+                    </div>
+                </div>
+                {
+                    showInfo.foursInfo ? 
+                        <div className={s.item__in}>
+                            <div className={s.item__info}>
+                                <div className={s.item}>
+                                    <label htmlFor="">Город</label>
+                                    <input disabled type="text" value={"Тбилиси"}/> 
+                                </div>
+                                <div className={s.item}>
+                                    <label htmlFor="">Улица</label>
+                                    <input disabled type="text" value={"Львови"}/> 
+                                </div>
+                                <div className={s.item}>
+                                    <label htmlFor="">Дом</label>
+                                    <input disabled type="text" value={"25"}/> 
+                                </div>
+                                <div className={s.item}>
+                                    <label htmlFor="">Квартира</label>
+                                    <input disabled type="text" value={"13"}/> 
+                                </div>
+                            </div>
+                        </div>
+                    : null
+                }
+            </div>
+            
         </div>
+        
     )
 }
 export default ProfileInfo
