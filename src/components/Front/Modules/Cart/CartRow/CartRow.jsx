6df0c96cd77b9e07b55fdcb31e294e84545bde7a
tahomi <tahomidev@gmail.com>
2023-06-26 18:04:34 +0300

    Fix icons

--- src/components/Front/Modules/Cart/CartRow/CartRow.jsx
+++ src/components/Front/Modules/Cart/CartRow/CartRow.jsx
@@ -76,10 +76,12 @@ const CartRow = (props) =>{
                         </div>
                     
                         <div className={s.delete}>
-                            <img src={deletebtn} alt={t('cartRow-deleteAlt')} onClick={()=>{
+                            <svg onClick={()=>{
                                 deleteById(product.id, state.varId)
                                 props.change()
-                            }}/>
+                            }} x="0px" y="0px" width="24" height="24" viewBox="0 0 24 24">
+                                <path d="M 10 2 L 9 3 L 3 3 L 3 5 L 21 5 L 21 3 L 15 3 L 14 2 L 10 2 z M 4.3652344 7 L 6.0683594 22 L 17.931641 22 L 19.634766 7 L 4.3652344 7 z"></path>
+                            </svg>
                         </div>
                     </div>
                 </div>
--- src/components/Modals/CartModal/CartModal.jsx
+++ src/components/Modals/CartModal/CartModal.jsx
@@ -27,9 +27,10 @@ const CartModal = ({close, }) =>{
             <div className={s.in}>
                 <div className={s.title}>
                     <h3>{t('frontCart-title')}</h3>
-                    <img src={chrest} alt={t('frontCart-crossAlt')} onClick={()=>{
-                        close()
-                    }} />
+                    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAv0lEQVR4nO2VXQrCMBAGxwtalZY86NkVLP6hN6hQCaQgJdXdsFHEDOQt+02TbBooFP6JBrgBB2ChqFsCJ+AK1FrpLBT2YXSAE9S5MHeo8xlqLk8BEvlY6sc5RbyKBE3JY9IuZCRRRwLvwGbUC7E561SpRJ5N+m4rpUdhvvJsK5XKs0qZONNYw31E2ueUu280V/Piykjuubl0wFxeGfwyfYaa1uCR2KeIjwbPos9QU4Uv3iq3zM/dhdp5irhQ+E0ekyummbane5EAAAAASUVORK5CYII="
+                         onClick={()=>{
+                             close()
+                         }} />
                 </div>
                 <div className={s.cart__mod}>
                     <CartModule setTotalPrice={(price)=>{
